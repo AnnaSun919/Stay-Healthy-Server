b9d44c5b855234df6b94eac5e104d9c69e8847fc
annasun919 <yuval_sun@yahoo.com.hk>
2021-07-24 18:18:04 +0200

    edit activity model

--- routes/activities.routes.js
+++ routes/activities.routes.js
@@ -6,12 +6,12 @@ router.post("/activity/create", (req, res) => {
   const { time, date, name, image, description, location, category, creater } =
     req.body;
 
-  // if (!name || !date || !time || !description) {
-  //   res.status(500).json({
-  //     errorMessage: "Please enter all field",
-  //   });
-  //   return;
-  // }
+  if (!name || !date || !time || !description || !location) {
+    res.status(500).json({
+      errorMessage: "Please enter all field",
+    });
+    return;
+  }
 
   ActivityModel.create({
     name: name,
@@ -34,4 +34,55 @@ router.post("/activity/create", (req, res) => {
       });
     });
 });
+
+router.get("/activities", (req, res) => {
+  console.log("hi");
+  ActivityModel.find()
+    .then((activity) => {
+      res.status(200).json(activity);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+});
+
+router.get("/activity/:id/edit", (req, res) => {
+  console.log("Hi");
+  id = req.params.id;
+
+  ActivityModel.findById(id)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+});
+
+router.patch("/activity/:id/edit", (req, res) => {
+  let id = req.params.id;
+  const { name, location, category } = req.body;
+  ActivityModel.findByIdAndUpdate(
+    id,
+    { $set: { name: name, location: location, category: category } },
+    { new: true }
+  )
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+});
+
 module.exports = router;
